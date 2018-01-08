f5721253fe126b5d0544dd06a67528ace16bac7a
Pranav Doshi <pranavdoshi14@gmail.com>
2018-01-08 19:34:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+function shout(string)
+{
+  return string.toUpperCase()
+}
+
+function whisper(String)
+{
+  return string.toLowerCase()
+}
+
+function logshout(string)
+{
+  console.log(string.toUpperCase())
+}
+
+function logwhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string)
+{
+  uppercase = string.toUpperCase()
+  if(uppercase === string.toUpperCase())
+  {
+    return "YES INDEED!"
+  }
+  else if(uppercase===string.toLowerCase())
+  {
+    return "I can't hear you!" 
+  }
+  else
+  {
+    return "I love you, too."
+  }
+}
